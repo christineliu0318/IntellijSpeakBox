1870ef710a84c03f432930404a78862c2ad5c70a
christineliu0318 <christineliu0318@gmail.com>
2016-04-01 10:45:28 -0700

    Add csv jar file and trying to get graph to show

--- Speakbox/src/com/example/SpeakBox/SpeakBox.java
+++ Speakbox/src/com/example/SpeakBox/SpeakBox.java
@@ -3,12 +3,19 @@ package com.example.SpeakBox;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.RadioButton;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import java.util.ArrayList;
 
@@ -20,7 +27,9 @@ public class SpeakBox extends Activity {
 
     private SharedPreferences settings = null;
     private SharedPreferences.Editor editor = null;
-    LineChart chart = (LineChart) findViewById(R.id.chart);
+    private LineChart chart = (LineChart) findViewById(R.id.chart);
+
+    private Typeface mTf;
 
     /**
      * Called when the activity is first created.
@@ -43,6 +52,18 @@ public class SpeakBox extends Activity {
         Log.v(TAG, "Starting CheckRecentRun service...");
         startService(new Intent(this,  CheckRecentRun.class));
 
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.chart);
+
+
+        mTf = Typeface.createFromAsset(getAssets(), "OpenSans-Bold.ttf");
+
+        LineData data = getData(36, 100);
+        data.setValueTypeface(mTf);
+        int color =  Color.rgb(137, 230, 81);
+        setupChart(chart, data, color);
     }
 
     public void recordRunTime() {
@@ -63,6 +84,98 @@ public class SpeakBox extends Activity {
 //        Log.v(TAG, "Notifications disabled");
 //    }
 
+    private void setupChart(LineChart chart, LineData data, int color) {
+
+        // no description text
+        chart.setDescription("");
+        chart.setNoDataTextDescription("You need to provide data for the chart.");
+
+        // mChart.setDrawHorizontalGrid(false);
+        //
+        // enable / disable grid background
+        chart.setDrawGridBackground(false);
+//        chart.getRenderer().getGridPaint().setGridColor(Color.WHITE & 0x70FFFFFF);
+
+        // enable touch gestures
+        chart.setTouchEnabled(true);
+
+        // enable scaling and dragging
+        chart.setDragEnabled(true);
+        chart.setScaleEnabled(true);
+
+        // if disabled, scaling can be done on x- and y-axis separately
+        chart.setPinchZoom(false);
+
+        chart.setBackgroundColor(color);
+
+        // set custom chart offsets (automatic offset calculation is hereby disabled)
+        chart.setViewPortOffsets(10, 0, 10, 0);
+
+        // add data
+        chart.setData(data);
+
+        // get the legend (only possible after setting data)
+        Legend l = chart.getLegend();
+        l.setEnabled(false);
+
+        chart.getAxisLeft().setEnabled(false);
+        chart.getAxisRight().setEnabled(false);
+
+        chart.getXAxis().setEnabled(false);
+
+        // animate calls invalidate()...
+        chart.animateX(2500);
+    }
+
+    private String[] mMonths = new String[] {
+            "Jan",
+            "Feb",
+            "Mar",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"
+    };
+
+    private LineData getData(int count, float range) {
+
+        ArrayList<String> xVals = new ArrayList<String>();
+        for (int i = 0; i < count; i++) {
+            xVals.add(mMonths[i % 12]);
+        }
+
+        ArrayList<Entry> yVals = new ArrayList<Entry>();
+
+        for (int i = 0; i < count; i++) {
+            float val = (float) (Math.random() * range) + 3;
+            yVals.add(new Entry(val, i));
+        }
+
+        // create a dataset and give it a type
+        LineDataSet set1 = new LineDataSet(yVals, "DataSet 1");
+        // set1.setFillAlpha(110);
+        // set1.setFillColor(Color.RED);
+
+        set1.setLineWidth(1.75f);
+        set1.setCircleRadius(3f);
+        set1.setColor(Color.WHITE);
+        set1.setCircleColor(Color.WHITE);
+        set1.setHighLightColor(Color.WHITE);
+        set1.setDrawValues(false);
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
+        dataSets.add(set1); // add the datasets
+
+        // create a data object with the datasets
+        LineData data = new LineData(xVals, dataSets);
+
+        return data;
+    }
     /**
      * This method is called when the submit button is clicked.
      */
@@ -91,13 +204,11 @@ public class SpeakBox extends Activity {
             //TODO: add it to a list of question answers
         } else if (radio4Pressed) {
             //TODO: add it to a list of question answers
-        } else {
+        } else if (radio5Pressed){
             //TODO
         }
 
-        LineData data = new LineData(new ArrayList<String>().add(xaxis),);
-        chart.setData(data);
-        chart.notifyDataSetChanged();
+
     }
 
 }
